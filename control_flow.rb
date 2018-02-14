d09808676b81007a3e921a58210bb3669b077993
Byron <jamesblocker81@gmail.com>
2018-02-14 12:23:52 -0800

    add control flow exercises

---
+++ control_flow.rb
@@ -0,0 +1,38 @@
+#Banjo
+def plays_banjo
+    puts "What is your name?"
+    name = gets.chomp.downcase
+    if name[0] == "r"
+        puts "#{name.capitalize} plays Banjo"
+    else
+        puts "#{name.capitalize} does not play Banjo"
+    end
+end
+#plays_banjo
+
+#Vowel Count
+def vowel_count
+    puts "Write something"
+    characters = gets.chomp.downcase
+    vowels_num = characters.count 'aieou'
+
+    if vowels_num > 0
+        puts "there were #{vowels_num} vowels in this statement"
+    else
+        puts "There were no vowels in this statement"
+    end
+end
+
+#vowel_count
+
+#Palindrome
+def palindrome
+    puts "Say something"
+    input = gets.chomp.downcase
+    if input == input.reverse
+        puts "PALINDROME!"
+    else
+        puts "Definitely not a palindrome"
+    end
+end
+#palindrome
---
+++ ruby_control_flow.rb
@@ -0,0 +1,11 @@
+its_sunny_outside = false
+im_awake = false
+im_hungry = true
+
+if 10
+    puts "Ready to learn! 🧠"
+else
+    puts "I need to go back to bed 😴"
+end
+
+#puts "Ready to learn" unless its_sunny_outside
