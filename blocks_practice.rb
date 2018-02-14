afa19c4afcc726db0abbb589687377c8b908657d
Byron <jamesblocker81@gmail.com>
2018-02-14 14:31:31 -0800

    create file for block practice

---
+++ blocks_practice.rb
@@ -0,0 +1,19 @@
+def jump_up
+    puts "Getting ready to jump up"
+    puts "Jumping up"
+    puts "End of the function"
+end
+
+def get_down
+    puts "Getting ready to get down"
+    puts "Getting down"
+    puts "End of function"
+end
+
+def jump_up_and_get_down
+    jump()
+    get_down()
+    puts "End of the function"
+end
+
+jump_up_and_get_down
