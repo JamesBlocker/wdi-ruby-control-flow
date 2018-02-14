4be788caf5d4f553bbed3e3da26ec41f4065b6d9
Byron <jamesblocker81@gmail.com>
2018-02-14 15:17:57 -0800

    build times function

--- blocks_practice.rb
+++ blocks_practice.rb
@@ -1,28 +1,46 @@
-def jump_around
-    puts "Getting ready to jump around"
-    yield
-    puts "End of the function"
-end
+# def jump_around
+#     puts "Getting ready to jump around"
+#     yield
+#     puts "End of the function"
+# end
 
-jump_around() do
-    puts "Jump up jump up and get down"
-end
+# jump_around() do
+#     puts "Jump up jump up and get down"
+# end
 
-flavors = ["chocolate", "Vanilla", "Strawberry"]
-# flavors.each do |flavor|
+# flavors = ["chocolate", "Vanilla", "Strawberry"]
+# # flavors.each do |flavor|
+# #     puts flavor
+# # end
+
+# class Array
+#     def backwards_each
+#         i = self.length - 1
+#         while i >= 0 do
+#             yield(self[i])
+#             i -= 1
+#         end
+#     end
+# end
+
+# flavors.backwards_each do |flavor|
 #     puts flavor
 # end
 
-class Array
-    def backwards_each
-        i = self.length - 1
-        while i >= 0 do
-            yield(self[i])
-            i -= 1
+# 10.times do
+#     puts "PRINT THIS SHIT YO"
+# end
+
+class Integer
+    def my_times
+        i = 0
+        while i < self do
+            yield
+            i += 1
         end
     end
 end
 
-flavors.backwards_each do |flavor|
-    puts flavor
+15.my_times do 
+    p "yallllooW"
 end
